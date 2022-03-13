4962e0a67a758c93a9863d84370d77715b3c00a4
azang0 <azang@mit.edu>
2022-03-13 04:51:57 +0000

    passes unit tests

--- src/localization/motion_model.py
+++ src/localization/motion_model.py
@@ -1,3 +1,7 @@
+import numpy as np
+import rospy
+
+
 class MotionModel:
 
     def __init__(self):
@@ -6,8 +10,10 @@ class MotionModel:
         # TODO
         # Do any precomputation for the motion
         # model here.
-
-        pass
+        self.deterministic = rospy.get_param('~deterministic', False)
+        self.x_scale = rospy.get_param('noise_scale_x', 1)
+        self.y_scale = rospy.get_param('noise_scale_y', 1)
+        self.theta_scale = rospy.get_param('noise_scale_theta', 1)
 
         ####################################
 
@@ -33,6 +39,17 @@ class MotionModel:
         ####################################
         # TODO
 
-        raise NotImplementedError
+        # raise NotImplementedError
+        predicted_particles = np.matrix.copy(particles)
+        thetas_cos = np.cos(particles[:,2])
+        thetas_sin = np.sin(particles[:,2])
+        predicted_particles[:,0] += odometry[0] * thetas_cos - odometry[1] * thetas_sin
+        predicted_particles[:,1] += odometry[0] * thetas_sin + odometry[1] * thetas_cos
+        predicted_particles[:,2] += odometry[2]
+        if not self.deterministic:
+            predicted_particles[:, 0] += np.random.normal(0, self.x_scale,particles.shape[0])
+            predicted_particles[:, 1] += np.random.normal(0, self.y_scale,particles.shape[0])
+            predicted_particles[:, 2] += np.random.normal(0, self.theta_scale,particles.shape[0])
+        return predicted_particles
 
         ####################################
